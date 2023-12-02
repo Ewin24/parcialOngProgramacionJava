28cc33721d74a152bc128c9d49aec2232763bc1d
ewin24 <edwintrigos24@gmail.com>
2023-12-02 17:33:17 -0500

    feat: :sparkles: New Queries for the repositories

--- src/main/java/com/example/demo/Repositories/EnvioRepository.java
+++ src/main/java/com/example/demo/Repositories/EnvioRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.Repositories;
 
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.Entities.EnvioEntity;
 
-public interface EnvioRepository extends CrudRepository<EnvioEntity, Long>{
-    
+public interface EnvioRepository extends CrudRepository<EnvioEntity, Long> {
+    @Query(value = "SELECT ID_SEDE FROM SEDE_ENVIO WHERE ID_ENVIO = ?1", nativeQuery = true)
+    List<Long> findSedesByIdEnvio(Long idEnvio);
 }
--- src/main/java/com/example/demo/Repositories/VoluntarioRepository.java
+++ src/main/java/com/example/demo/Repositories/VoluntarioRepository.java
@@ -1,9 +1,22 @@
 package com.example.demo.Repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.Entities.VoluntarioEntity;
 
 public interface VoluntarioRepository extends CrudRepository<VoluntarioEntity, Long> {
 
+    @Modifying
+    @Query(value = "UPDATE SEDE SET ID_VOLUNTARIO_JEFE = NULL WHERE ID_VOLUNTARIO_JEFE = ?1", nativeQuery = true)
+    void deleteJefeWithSede(Long idVoluntario);
+
+    @Query(value = "SELECT * FROM VOLUNTARIO WHERE ID_SEDE = ?1", nativeQuery = true)
+    List<VoluntarioEntity> findByIdSede(Long idSede);
+
+    @Query(value = "SELECT * FROM VOLUNTARIO WHERE LOWER(PROFESION) = LOWER(?1)", nativeQuery = true)
+    List<VoluntarioEntity> findByProfesion(String profesion);
 }
